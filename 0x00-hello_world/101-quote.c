8638e9be3b45d493ad55e2695435f9942110eab7
Hassan <hassanpositive@gmail.com>
2023-06-15 08:42:37 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <unistd.h>
 
 /**
-* main - Entry point
+* main - A c program that print a line to the standard error
 *
-* Return Always 1 (Success)
+* Return 1 (Success)
 */
 int main(void)
 {
-	write(2,
-	“and that piece of art is useful\”- Dora Korpar,2015-10-19\n”, 59)
+	fput(“and that piece of art is useful\” - Dora Korpar, 2015-10-19\n”, stdout);
 	return (1);
 }
