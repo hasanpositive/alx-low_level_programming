c029ecf76f55e9b05f9ed9dc0d7a6a40375e48ae
Hassan <hassanpositive@gmail.com>
2023-06-15 22:27:43 -0700

    Q8 correct code is here: #include <stdio.h> #include <unistd.h>
    /**
     * main - Entry point
     *
     * Return: Always 0 (Success)
     */
    int main(void)
    {
    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
    return (1);
    />
    .
    .
    \
    "
    ">

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A c program that print lines to the standard
- * Return 1 (Sucess)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-int main(void){
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
