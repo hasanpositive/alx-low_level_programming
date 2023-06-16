709a5e7ab63a34711a778948cff6548efa3369f9
Hassan <hassanpositive@gmail.com>
2023-06-15 21:34:27 -0700

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
+#include <stdio.h>
 #include <unistd.h>
-
 /**
-* main - A program that prints 
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
-{
-	write(2, “and that piece of art is useful\” - Dora Korpar, 2015-10-19\n”, 59);
+ * main - A c program that print lines to the standard
+ * Return 1 (Sucess)
+ */
+int main(void){
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
