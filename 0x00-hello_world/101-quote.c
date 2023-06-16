78156ab4f2865eaf7be8be9016b82745057c414c
Hassan <hassanpositive@gmail.com>
2023-06-15 20:57:39 -0700

    upd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - A program to determine size of datatypes
- *
- * Return: 1 (not success)
+* main - A program that prints 
+*
+* Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	char quo[] = “and that piece of art is useful\” - Dora Korpar, 2015-10-19\n”
-
-write(1,quo,59);
+	write(2, “and that piece of art is useful\” - Dora Korpar, 2015-10-19\n”, 59);
 	return (1);
 }
