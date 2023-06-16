1750941269c758251993b70096d290e9d64b5622
Hassan <hassanpositive@gmail.com>
2023-06-15 20:04:34 -0700

    las

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,2 @@
-#include <stdio.h>
-#include <unistd.h>
-
-/**
-* main - A c program that print a line to the standard error
-*
-* Return 1 (Success)
-*/
-int main(void)
-{
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
-}
-
+#!/bin/bash
+gcc -S -masm=intel $CFILE -o `echo $CFILE | cut -d. -f1`.s
