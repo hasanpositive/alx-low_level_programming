fb5014ddd11d4baffab9e1e75e72a8fc90bac9cc
Hassan <hassanpositive@gmail.com>
2023-06-21 00:48:22 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -23,7 +22,7 @@ void print_times_table(int n)
 			if (prod <= 99)
 				_putchar(' ');
 			if (prod <= 9)
-			i	_putchar(' ');
+				_putchar(' ');
 			if (prod >= 100)
 			{
 				_putchar((prod / 100) + '0');
