4066a8a497077486a29c2ab5092016700cb94a36
Hassan <hassanpositive@gmail.com>
2023-06-23 01:50:14 -0700

    Excess function parameter size description in print_square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - Print a square followed by a new line
+ * Return: empty
+ */
+void print_square(void)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
