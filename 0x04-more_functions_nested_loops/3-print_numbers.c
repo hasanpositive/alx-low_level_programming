cf039b3c869410b7d1898d73aa4ea2f1429e220e
Hassan <hassanpositive@gmail.com>
2023-06-22 22:24:44 -0700

    update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - Print the numbers since 0 up to 9
- * Returnd: return the numbers since 0 to 9
+ * print_numbers - Print 0 up to 9
+ * Returnd: void
  */
 void print_numbers(void)
 {
-	int x;
+	char c;
 
-	for (x = 0; x <= 9; x++)
+	for (c = '0'; c <= '9'; x++)
 	{
-		_putchar(x + '0');
+		_putchar(c);
 	}
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,16 @@
 #include "main.h"
-
 /**
- * print_most_numbers - Print the numbers from 0 to 9
- * Description: Print the numbers excluding 2 and 4
- * Return: The numbers from 0 upto 9
+ * print_most_numbers - Print numbers except 2 and 4
+ * Return: void
  */
 void print_most_numbers(void)
 {
-	int x = 0;
+char c;
 
-	for (; x <= 9; x++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (x == 2 || x == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(x + '0');
-		}
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
 	_putchar('\n');
 }
